ffded322ab6cb706696721056a5d4ef89dab9d7b
Rohank022 <125291679+Rohank022@users.noreply.github.com>
2025-03-06 17:01:07 +0530

    Added splash screen

---
+++ app/src/main/java/com/wristcandy/splashScreen.java
@@ -0,0 +1,37 @@
+package com.wristcandy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 3000; // 3 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Delay and move to MainActivity
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(splashScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // Close splash screen
+        }, SPLASH_DELAY);
+    }
+}
